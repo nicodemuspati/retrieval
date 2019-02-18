85eacc4629beea759a54f09fbd48ca699640b540
Nico <>
2019-02-18 18:58:47 +0700

    service V 2.8

---
+++ retrievel/src/model/Document.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Document {
+   
+    private int id;
+    private String content;
+
+    public Document() {
+    }
+
+    public Document(String content) {
+        this.content = content;
+    }
+
+    public Document(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Document(int i, String computer_information_retrieval) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String[] getListofTerm(){
+        return null;
+    }
+    
+    }}
+
+    
+
