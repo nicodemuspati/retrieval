54881e6141348760d509e22b5efbfb617bc34a22
Nico <ASUS@X454Y>
2019-02-26 07:23:55 +0700

    service V 3.1

--- retrievel/src/test/testDocument1.java
+++ retrievel/src/test/testDocument1.java
@@ -13,16 +13,14 @@ import model.Document;
  */
 public class testDocument1 {
     public static void main(String[] args) {
-        Document doc1 = new Document(1, "computer information retrieval.");
-        Document doc2 = new Document(2, "computer organization and architecture");
+        Document doc1 = new Document(1, "computer information retrieval");
+        Document doc2 = new Document(2, "computer oraganization and architecture");
+        
         String result[] = doc1.getListofTerm();
         for (int i = 0; i < result.length; i++) {
-            System.out.println("term "+i+" = "+result[i]);
+            System.out.println("term " + i + " = " + result[i]);
         }
-        String result1[] = doc2.getListofTerm();
-        for (int i = 0; i < result1.length; i++) {
-            System.out.println("term "+i+" = "+result1[i]);
-        }
+        
         
     }
 }
